e94aaeb011490c8951d043343f2d5983d1de4133
Joachim Happel <joachim.happel@gmail.com>
2022-05-02 17:48:15 +0200

    fix Frontent Output

--- controls/bausteine.php
+++ controls/bausteine.php
@@ -19,18 +19,33 @@ class bausteine {
             .baustein-gallery{
                 margin: 0;
             }
+            .baustein-gallery-header{
+                display:grid;
+                grid-template-columns: 60px auto;
+                grid-auto-rows: minmax(40px, auto);
+                margin: 20px 0 10px;
+            }
+
+            .baustein-gallery-header svg{
+                margin-bottom: -24px;
+            }
+            .baustein-gallery .block-title{
+                font-weight: normal;
+                font-size: 30px;
+                margin: 0;
+            }
+
+            .baustein-gallery .card-title{
+                margin: 0;
+                font-size: 24px;
+            }
             .baustein-gallery-grid{
                 display:grid;
                 grid-gap: 10px;
                 grid-template-columns: repeat(3, 1fr);
                 grid-auto-rows: minmax(40px, auto);
                 margin: 0;
-                transition: all .5s linear;
 
-            }
-            .baustein-gallery-grid h4{
-                font-size: 24px;
-                transition: all .5s linear;
             }
             .baustein-footer{
                 text-align: right;
@@ -38,51 +53,95 @@ class bausteine {
             .scaled .baustein-gallery-grid{
                 /*grid-template-columns: repeat(6, 1fr);*/
                 width: 100%;
-                transition: all .5s linear;
             }
             .scaled .baustein-gallery-grid p{
-                display: none;
+                font-size: 0px;
+                line-height: 0px;
+                transition: all .5s linear;
             }
 
-            .scaled .baustein-gallery-grid h4{
+            .scaled.baustein-gallery .card-title{
                 font-size: 20px;
                 transition: all .5s linear;
             }
-            .scaled .baustein-gallery-grid .baustein-card{
-                overflow: hidden;
+
+            .scaled .baustein-card{
                 transition: all .5s linear;
+                height: 50px;
+                overflow: hidden;
             }
             .scaled .baustein-footer{
                 display: none;
+                transition: all .5s linear;
+            }
+            div.selected-card{
+                border:1px solid orangered;
+                background: #fff;
+            }
+
+            .baustein-gallery .showroom-wrapper{
+                border-right: 5px;
+                margin-top: 10px;
+                position: static;
+            }
+
+            .baustein-gallery .showroom{
+                position: relative;
             }
 		</style>
 
 		<div class="baustein-gallery" data-block="<?php echo $attributes['blockId'] ?>">
             <div class="baustein-gallery-header">
-                <h3><?php echo $attributes['sammlung'];?></h3>
+                <h3 class="block-title"><?php echo $attributes['sammlung'];?></h3>
             </div>
 			<div class="baustein-gallery-grid">
 				<?php echo $attributes['bausteine'];?>
 			</div>
-            <div class="showroom"></div>
+            <div class="showroom-wrapper">
+                    <div class="showroom"></div>
+            </div>
 		</div>
         <script>
             jQuery(document).ready(($)=>{
+
+                const icon = $('<div class="bausteine-icon"><svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><g><g><path d="M3,3v8h8V3H3z M9,9H5V5h4V9z M3,13v8h8v-8H3z M9,19H5v-4h4V19z M13,3v8h8V3H13z M19,9h-4V5h4V9z M13,13v8h8v-8H13z M19,19h-4v-4h4V19z"/></g></g></g></svg></div>');
+                $('.baustein-gallery-header').prepend(icon);
+
                 $('.showroom').slideUp();
+
                 $('.baustein-card').on('click', (e)=>{
-                    $('.showroom').fadeOut();
+
                     if($(e.target).hasClass('baustein-card')){
                         card = $(e.target);
                     }else{
                         card = $(e.target).closest('.baustein-card');
                     }
-                    let gallery =  card.closest('.baustein-gallery');
-                    gallery.addClass('scaled');
-                    let showroom = gallery.find('.showroom') ;
-                    console.log('showroom',showroom);
-                    showroom.html(card.find('.baustein-inner-content').html());
-                    setTimeout(()=>{showroom.slideDown();},500);
-                    showroom.fadeIn();
+
+                    if(card.hasClass('selected-card')){
+                        $('.baustein-card').removeClass('selected-card');
+                        $('.showroom').slideUp();
+                        setTimeout(()=>{  $('.baustein-gallery').removeClass('scaled');},500);
+
+                    }else{
+                        $('.baustein-card').removeClass('selected-card');
+                        card.addClass('selected-card');
+                        let gallery =  card.closest('.baustein-gallery');
+                        gallery.addClass('scaled');
+                        let showroom = gallery.find('.showroom') ;
+                        showroom.html(card.find('.baustein-inner-content').html());
+                        showroom.find('.baustein-article').show();
+                        showroom.slideDown();
+
+                        //von rechts einfliegen
+                        let h = showroom.height()+'px';
+                        showroom.css({'min-height':h});
+                        showroom.find('.baustein-article').css({'position':'absolute','left':'300px', 'opacity':0});
+                        showroom.find('.baustein-article').animate({'left':'1px','opacity':1},300,()=>{
+                            showroom.find('.baustein-article').css({'position':'relative'});
+                            showroom.css({'min-height':'unset'});
+                        });
+
+                    }
 
 
                 })
@@ -93,8 +152,10 @@ class bausteine {
                     // if the target of the click isn't the container nor a descendant of the container
                     if (!container.is(e.target) && container.has(e.target).length === 0)
                     {
+                        $('.baustein-gallery').removeClass('scaled');
+                        $('.baustein-card').removeClass('selected-card');
                         $('.showroom').slideUp();
-                        setTimeout(()=>{  $('.baustein-gallery').removeClass('scaled');},500);
+
                     }
 
                 });
@@ -112,27 +173,67 @@ class baustein {
         <style>
             .baustein-card{
                 display: flex;
-                border: 1px solid #ddd;
+                border: 1px solid #aaa;
                 border-radius: 5px;
                 padding: 10px;
                 flex-direction: column;
                 height: 100%;
                 justify-content: space-between;
+                box-shadow: 1px 1px 3px #999;
+                transition: all .3s linear;
+            }
+            .baustein-card:hover{
+                border: 1px solid orange;
+                background: lavenderblush;
+                box-shadow: 1px 1px 5px red;
+            }
+            .baustein-article{
+                margin: 1px;
+                border: 1px solid orangered;
+                border-radius: 5px;
+                padding: 10px;
+                background: #fff;
+                box-shadow: 1px 1px 5px #555555;
+                width: 100%;
+            }
+            .baustein-card p{
+                transition: all .3s linear;
+            }
+            .baustein-article-header{
+                display:grid;
+                grid-template-columns: 60px auto;
+                grid-auto-rows: minmax(30px, auto);
+                margin: 0px;
+            }
+            .baustein-article-header .block-title{
+                font-size: 30px;
+            }
+            .baustein-article-header .baustein-icon{
+                width: 40px;
+                margin-top: 3px;
+            }
+            .baustein-article-header .baustein-icon svg {
+                width: 100%;
+                height: auto;
             }
             .baustein-inner-content{
                 display: none;
-                position: absolute;
-                background: aliceblue;
-
             }
         </style>
         <div class="baustein-card <?php echo $attributes['post_id']?'post':'';?>" id="bcard-<?php echo $attributes['blockId'] ?>" data-block="<?php echo $attributes['blockId'] ?>">
-            <h4><?php echo $attributes['titel'];?></h4>
+            <div class="card-title"><?php echo $attributes['titel'];?></div>
             <p><?php echo $attributes['kurzbeschreibung'];?></p>
             <div class="baustein-inner-content">
-                <h4><?php echo $attributes['titel'];?></h4>
-                <p><?php echo $attributes['kurzbeschreibung'];?></p>
-                <?php echo $attributes['content'];?>
+                <div class="baustein-article">
+                    <div class="baustein-article-header">
+                        <div class="baustein-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16h-9v-6h9v6z" /></svg>
+                        </div>
+                        <h4 class="block-title"><?php echo $attributes['titel'];?></h4>
+                    </div>
+                    <p><?php echo $attributes['kurzbeschreibung'];?></p>
+                    <?php echo $attributes['content'];?>
+                </div>
             </div>
             <div>
 
--- lzb-post-selector.php
+++ lzb-post-selector.php
@@ -88,10 +88,7 @@ class rpi_Lzb_Plugin_Post_Selector {
 	static function blockeditor_engueue(){
 
 		if (!is_admin()) return;
-		/*wp_enqueue_script(
-			'template_handling',
-			plugin_dir_url( __FILE__ ) . '/assets/js/template_handling_editor.js'
-		);*/
+
 		wp_enqueue_style(
 			'modal-posts_style',
 			plugin_dir_url( __FILE__ ) . '/assets/css/thickbox-content.css'
@@ -100,10 +97,6 @@ class rpi_Lzb_Plugin_Post_Selector {
 			'bausteine_style',
 			plugin_dir_url( __FILE__ ) . '/assets/css/bausteine.css'
 		);
-//		wp_enqueue_style(
-//			'zebra_dialog_style',
-//			plugin_dir_url( __FILE__ ) . '/assets/css/zebra_dialog/flat/zebra_dialog.min.css'
-//		);
 
 		wp_enqueue_script(
 			'bausteine',
@@ -117,10 +110,6 @@ class rpi_Lzb_Plugin_Post_Selector {
 			'jquery-sortable',
 			plugin_dir_url( __FILE__ ) . '/assets/js/jquery-sortable.js'
 		);
-//		wp_enqueue_script(
-//			'zebra_dialog',
-//			plugin_dir_url( __FILE__ ) . '/assets/js/zebra_dialog.min.js'
-//		);
 	}
 }
 
